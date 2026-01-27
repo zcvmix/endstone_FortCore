51319ce470f6bb88069b96d255b401e546636164
dead <250851814+zcvmix@users.noreply.github.com>
2026-01-28 05:10:34 +0800

    Update __init__.py

--- src/endstone_fortcore/__init__.py
+++ src/endstone_fortcore/__init__.py
@@ -1,9 +1,10 @@
 """
 FortCore - High-performance PvP Core Plugin for Endstone
+Optimized for low-end servers and devices
 """
 
 from endstone_fortcore.fortcore import FortCore
 from endstone_fortcore.rollback import RollbackManager, GameState, PlayerData, RollbackAction
 
 __all__ = ["FortCore", "RollbackManager", "GameState", "PlayerData", "RollbackAction"]
-__version__ = "1.1.0"
+__version__ = "1.2.0"
